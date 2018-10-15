1bb7f24303bf18c91cad272087ea272a8f9aabba
WarleyLopes <warleylopes@outlook.com>
2018-10-15 16:11:22 -0300

    Adding property tags of NuGet package info details directly into csproj file

--- src/Selenium.QuickStart/Selenium.QuickStart/Properties/AssemblyInfo.cs
+++ src/Selenium.QuickStart/Selenium.QuickStart/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Warley Ferreira Lopes")]
 [assembly: AssemblyProduct("Selenium.QuickStart")]
-[assembly: AssemblyCopyright("MIT License Copyright (c) 2018 Warley")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 Warley")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o '*' como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.5")]
+[assembly: AssemblyFileVersion("0.5")]
 [assembly: NeutralResourcesLanguage("")]
 
