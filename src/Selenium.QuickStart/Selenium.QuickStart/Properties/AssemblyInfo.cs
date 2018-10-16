3266ad0a3dd59052ff19a9584d5b809c1873422c
WarleyLopes <warleylopes@outlook.com>
2018-10-16 11:35:46 -0300

    Removing reference to System.Drawing needed for Selenium.WebDriver to later try to add it again to try to fix the reference dependency

--- src/Selenium.QuickStart/Selenium.QuickStart/Properties/AssemblyInfo.cs
+++ src/Selenium.QuickStart/Selenium.QuickStart/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o '*' como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.3")]
-[assembly: AssemblyFileVersion("0.6.3")]
+[assembly: AssemblyVersion("0.6.3.1")]
+[assembly: AssemblyFileVersion("0.6.3.1")]
 [assembly: NeutralResourcesLanguage("")]
 
