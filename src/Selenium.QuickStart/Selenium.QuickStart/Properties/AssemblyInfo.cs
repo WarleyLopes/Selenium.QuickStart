091e5254fb6d7441a11b951f1fdbbcdb9b568b70
WarleyLopes <warleylopes@outlook.com>
2018-10-16 17:45:45 -0300

    Changing the verssion number

--- src/Selenium.QuickStart/Selenium.QuickStart/Properties/AssemblyInfo.cs
+++ src/Selenium.QuickStart/Selenium.QuickStart/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o '*' como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.3.5")]
-[assembly: AssemblyFileVersion("0.6.3.5")]
+[assembly: AssemblyVersion("0.6.3.6")]
+[assembly: AssemblyFileVersion("0.6.3.6")]
 [assembly: NeutralResourcesLanguage("")]
 
