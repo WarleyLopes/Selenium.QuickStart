a957ae2921e84dcc8aeaf255d6773b98fd5c5939
WarleyLopes <warleylopes@outlook.com>
2018-10-24 16:53:23 -0300

    Change on build settings of the csproj

--- src/Selenium.QuickStart/Properties/AssemblyInfo.cs
+++ src/Selenium.QuickStart/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("4b9f79bb-dc37-4963-9af6-197dd0d89736")]
 
-[assembly: AssemblyVersion("0.7.6")]
-[assembly: AssemblyFileVersion("0.7.6")]
+[assembly: AssemblyVersion("0.7.7")]
+[assembly: AssemblyFileVersion("0.7.7")]
