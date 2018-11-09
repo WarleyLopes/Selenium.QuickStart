5c406f687fe935f8bb469f79caac131ba1875771
WarleyLopes <warleylopes@outlook.com>
2018-11-09 16:53:16 -0200

    Inclusion of functions to execute database queries and to get data from CSV files

---
+++ src/Selenium.QuickStart/Utilities/CsvReader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Selenium.QuickStart.Utilities
+{
+    /// <summary>
+    /// Static class for reading data from a default formatted CSV file (; delimiter and lines divided with carriage return and linefeed.)
+    /// </summary>
+    public static class CsvReader
+    {
+        /// <summary>
+        /// Method to return data from a CSV file into a list of arrays of strings so you can build data driven with an IEnumerable with yield return new TestCaseData
+        /// </summary>
+        /// <param name="filename">The filename of your CSV file that should be at your solution / project folder</param>
+        /// <param name="hasHeader">Optional parameter with default on true that defines if your CSV file has a header or not for skipping it into the tests</param>
+        /// <returns>Returns a list of arrays of strings so you can implement data driven with an Enumerable with yield return new TestCaseData</returns>
+        public static List<string[]> GetData(string filename, bool hasHeader = true)
+        {
+            List<string[]> data = new List<string[]>();
+            string csvContent = new StreamReader(filename.Replace(".csv", "") + ".csv").ReadToEnd();
+            MatchCollection matches = new Regex("(((?!\").)*\n)|(.*\".*\r\n.*\"\r\n)|(.*[^\"])").Matches(csvContent);
+            foreach (Match m in matches)
+            {
+                if (hasHeader & matches[0] == m) { }
+                else
+                    data.Add(m.Value.Split(';'));
+            }
+            return data;
+        }
+    }
+}
---
+++ src/Selenium.QuickStart/Utilities/DatabaseSqlFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace Selenium.QuickStart.Utilities
+{
+    /// <summary>
+    /// Static class to make access to the method of query execution into a database of a connection string "DatabaseConnectionString" or a Mydatabase.sqlite file at your solution / project folder
+    /// </summary>
+    public static class DatabaseSqlFactory
+    {
+        /// <summary>
+        /// Executes a query into a database with connection defiend on a connection string "DatabaseConnectionString" or a Mydatabase.sqlite file at your solution / project folder
+        /// </summary>
+        /// <param name="sql">The sql query you want to execute</param>
+        /// <returns>Returns a list of arrays of strings</returns>
+        public static List<string[]> ExecuteQuery(string sql)
+        {
+            dynamic command;
+
+            if (System.IO.File.Exists("MyDatabase.sqlite"))
+            {
+                SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                m_dbConnection.Open();
+                command = new SQLiteCommand(sql, m_dbConnection);
+            }
+            else
+            {
+                SqlConnection m_dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString);
+                m_dbConnection.Open();
+                command = new SqlCommand(sql, m_dbConnection);
+            }
+
+            DataSet ds = new DataSet();
+            List<string[]> lista = new List<string[]>();
+            DataTable table = new DataTable();
+            table.Load(command.ExecuteReader());
+            ds.Tables.Add(table);
+            command.Connection.Close();
+
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                return null;
+            }
+            try
+            {
+                for (int r = 0; r < ds.Tables[0].Rows.Count; r++)
+                {
+                    string[] a = new string[ds.Tables[0].Columns.Count];
+                    for (int c = 0; c < ds.Tables[0].Columns.Count; c++)
+                    {
+                        a[c] = ds.Tables[0].Rows[r][c].ToString();
+                    }
+                    lista.Add(a);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+            return lista;
+        }
+    }
+}
