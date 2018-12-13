d016d46a77a66de1c5426eeee307a64068d89682
WarleyLopes <warleylopes@outlook.com>
2018-12-13 17:17:04 -0200

    Adaptation of CsvReader.GetData() to expect a file from the Properties.Resources of a project

--- src/Selenium.QuickStart/Utilities/CsvReader.cs
+++ src/Selenium.QuickStart/Utilities/CsvReader.cs
@@ -12,13 +12,12 @@ namespace Selenium.QuickStart.Utilities
         /// <summary>
         /// Method to return data from a CSV file into a list of arrays of strings so you can build data driven with an IEnumerable with yield return new TestCaseData
         /// </summary>
-        /// <param name="filename">The filename of your CSV file that should be at your solution / project folder</param>
+        /// <param name="csvContent">A CSV file from your Properties.Resources</param>
         /// <param name="hasHeader">Optional parameter with default on true that defines if your CSV file has a header or not for skipping it into the tests</param>
         /// <returns>Returns a list of arrays of strings so you can implement data driven with an Enumerable with yield return new TestCaseData</returns>
-        public static List<string[]> GetData(string filename, bool hasHeader = true)
+        public static List<string[]> GetData(string csvContent, bool hasHeader = true)
         {
             List<string[]> data = new List<string[]>();
-            string csvContent = new StreamReader(filename.Replace(".csv", "") + ".csv").ReadToEnd();
             MatchCollection matches = new Regex("(((?!\").)*\n)|(.*\".*\r\n.*\"\r\n)|(.*[^\"])").Matches(csvContent);
             foreach (Match m in matches)
             {
